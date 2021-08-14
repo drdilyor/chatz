e782fb27cc336fc9706862168f8e3ce316fd22f6
drdilyor <drdilyor@outlook.com>
2021-08-14 21:00:39 +0500

    fix don't show loading if no messages

--- chat/views.py
+++ chat/views.py
@@ -31,18 +31,23 @@ def to_json(message: models.Message):
 def messages(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
         try:
-            offset = int(request.GET.get('offset', 0))
+            offset = int(request.GET['offset'])
         except ValueError:
             return HttpResponseBadRequest()
-        # hack
-        new_messages = []
-        for i in range(15):  # 15 seconds
-            new_messages = [to_json(i) for i in
-                            models.Message.objects.filter(id__gte=offset)]
-            if new_messages:
-                break
-            sleep(1)
-        return JsonResponse(new_messages, safe=False)
+        except KeyError:
+            return JsonResponse([
+                to_json(i) for i in models.Message.objects.all()
+            ], safe=False)
+        else:
+            # hack
+            new_messages = []
+            for i in range(15):  # 15 seconds
+                new_messages = [to_json(i) for i in
+                                models.Message.objects.filter(id__gte=offset)]
+                if new_messages:
+                    break
+                sleep(1)
+            return JsonResponse(new_messages, safe=False)
     elif request.method == 'POST':
         body = json.loads(request.body)
         text = body.get('text', '')
@@ -54,6 +59,6 @@ def messages(request: HttpRequest) -> HttpResponse:
             owner=request.user,  # noqa
         )
         message.save()
-        return JsonResponse(to_json(message), safe=True)
-    return JsonResponse(None, status=401, safe=True)
+        return JsonResponse(to_json(message), safe=False)
+    return JsonResponse(None, status=401, safe=False)
 
